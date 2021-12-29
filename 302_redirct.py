ba81cefdaa51283869efdcb71e37a914e4910f85
Ron Fury <28007909+ronfury@users.noreply.github.com>
2021-12-30 02:22:53 +0530

    Create 302_redirct.py

---
+++ 302_redirct.py
@@ -0,0 +1,25 @@
+
+#To test if redirects work for testing bypasses.
+#Usage: python3 302_redirect.py port “gopher://mycollaboratorurl/”
+
+#!/usr/bin/env python3
+
+import sys
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+if len(sys.argv)-1 != 2:
+    print("""
+Usage: {} <port_number> <url>
+    """.format(sys.argv[0]))
+    sys.exit()
+
+class Redirect(BaseHTTPRequestHandler):
+   def do_GET(self):
+       self.send_response(302)
+       self.send_header('Location', sys.argv[2])
+       self.end_headers()
+   def send_error(self, code, message=None):
+       self.send_response(302)
+       self.send_header('Location', sys.argv[2])
+       self.end_headers()
+HTTPServer(("", int(sys.argv[1])), Redirect).serve_forever()
